b79b5333edd4b0a347ac2e53922c052ee35a2d90
doky <nokernel@gmail.com>
2014-02-20 18:15:19 +0100

    Change ES Endpoint & metrics mapping for ES 1.0

--- elasticsearch.py
+++ elasticsearch.py
@@ -4,6 +4,7 @@ import collectd
 import json
 import urllib2
 import socket
+import collections
 
 global URL, STAT, PREFIX, ES_HOST, ES_PORT, ES_CLUSTER
 
@@ -12,96 +13,106 @@ ES_CLUSTER = "elasticsearch"
 ES_HOST = "localhost"
 ES_PORT = 9200
 VERBOSE_LOGGING = False
-STAT=dict()
 
-# INDICES METRICS #
-## CACHE
-STAT['indices.cache.field.eviction'] = { "type": "counter", "path": "nodes.%s.indices.cache.field_evictions" }
-STAT['indices.cache.field.size'] = { "type": "bytes", "path": "nodes.%s.indices.cache.field_size_in_bytes" }
-STAT['indices.cache.filter.count'] = { "type": "counter", "path": "nodes.%s.indices.cache.filter_count" }
-STAT['indices.cache.filter.evictions'] = { "type": "counter", "path": "nodes.%s.indices.cache.filter_evictions" }
-STAT['indices.cache.filter.size'] = { "type": "bytes", "path": "nodes.%s.indices.cache.filter_size_in_bytes" }
+Stat = collections.namedtuple('Stat', ('type', 'path'))
 
-## DOCS
-STAT['indices.docs.count'] = { "type": "gauge", "path": "nodes.%s.indices.docs.count" }
-STAT['indices.docs.deleted'] = { "type": "counter", "path": "nodes.%s.indices.docs.deleted" }
+STATS = {
 
-## FLUSH
-STAT['indices.flush.total'] = { "type": "counter", "path": "nodes.%s.indices.flush.total" }
-STAT['indices.flush.time'] = { "type": "counter", "path": "nodes.%s.indices.flush.total_time_in_millis" }
+    ## DOCS
+    'indices.docs.count': Stat("gauge", "nodes.%s.indices.docs.count"),
+    'indices.docs.deleted': Stat("counter", "nodes.%s.indices.docs.deleted"),
 
-## GET
-STAT['indices.get.exists-time'] = { "type": "counter", "path": "nodes.%s.indices.get.exists_time_in_millis" }
-STAT['indices.get.exists-total'] = { "type": "counter", "path": "nodes.%s.indices.get.exists_total" }
-STAT['indices.get.time'] = { "type": "counter", "path": "nodes.%s.indices.get.time_in_millis" }
-STAT['indices.get.total'] = { "type": "counter", "path": "nodes.%s.indices.get.total" }
-STAT['indices.get.missing-time'] = { "type": "counter", "path": "nodes.%s.indices.get.missing_time_in_millis" }
-STAT['indices.get.missing-total'] = { "type": "counter", "path": "nodes.%s.indices.get.missing_total" }
+    ## STORE
+    'indices.store.size': Stat("bytes", "nodes.%s.indices.store.size_in_bytes"),
+    'indices.store.throttle-time': Stat("counter", "nodes.%s.indices.store.throttle_time_in_millis"),
 
-## INDEXING
-STAT['indices.indexing.delete-time'] = { "type": "counter", "path": "nodes.%s.indices.indexing.delete_time_in_millis" }
-STAT['indices.indexing.delete-total'] = { "type": "counter", "path": "nodes.%s.indices.indexing.delete_total" }
-STAT['indices.indexing.index-time'] = { "type": "counter", "path": "nodes.%s.indices.indexing.index_time_in_millis" }
-STAT['indices.indexing.index-total'] = { "type": "counter", "path": "nodes.%s.indices.indexing.index_total" }
+    ## INDEXING
+    'indices.indexing.index-total': Stat("counter", "nodes.%s.indices.indexing.index_total"),
+    'indices.indexing.index-time': Stat("counter", "nodes.%s.indices.indexing.index_time_in_millis"),
+    'indices.indexing.index-current': Stat("counter", "nodes.%s.indices.indexing.index_current"),
+    'indices.indexing.delete-total': Stat("counter", "nodes.%s.indices.indexing.delete_total"),
+    'indices.indexing.delete-time': Stat("counter", "nodes.%s.indices.indexing.delete_time_in_millis"),
+    'indices.indexing.delete-current': Stat("counter", "nodes.%s.indices.indexing.delete_current"),
 
-## MERGES
-STAT['indices.merges.current'] = { "type": "gauge", "path": "nodes.%s.indices.merges.current" }
-STAT['indices.merges.current-docs'] = { "type": "gauge", "path": "nodes.%s.indices.merges.current_docs" }
-STAT['indices.merges.current-size'] = { "type": "bytes", "path": "nodes.%s.indices.merges.current_size_in_bytes" }
-STAT['indices.merges.total'] = { "type": "counter", "path": "nodes.%s.indices.merges.total" }
-STAT['indices.merges.total-docs'] = { "type": "gauge", "path": "nodes.%s.indices.merges.total_docs" }
-STAT['indices.merges.total-size'] = { "type": "bytes", "path": "nodes.%s.indices.merges.total_size_in_bytes" }
-STAT['indices.merges.time'] = { "type": "counter", "path": "nodes.%s.indices.merges.total_time_in_millis" }
+    ## GET
+    'indices.get.total': Stat("counter", "nodes.%s.indices.get.total"),
+    'indices.get.time': Stat("counter", "nodes.%s.indices.get.time_in_millis"),
+    'indices.get.exists-total': Stat("counter", "nodes.%s.indices.get.exists_total"),
+    'indices.get.exists-time': Stat("counter", "nodes.%s.indices.get.exists_time_in_millis"),
+    'indices.get.missing-total': Stat("counter", "nodes.%s.indices.get.missing_total"),
+    'indices.get.missing-time': Stat("counter", "nodes.%s.indices.get.missing_time_in_millis"),
+    'indices.get.current': Stat("counter", "nodes.%s.indices.get.current"),
 
-## REFRESH
-STAT['indices.refresh.total'] = { "type": "counter", "path": "nodes.%s.indices.refresh.total" }
-STAT['indices.refresh.time'] = { "type": "counter", "path": "nodes.%s.indices.refresh.total_time_in_millis" }
+    ## SEARCH
+    'indices.search.open-contexts': Stat("counter", "nodes.%s.indices.search.open_contexts"),
+    'indices.search.query-current': Stat("gauge", "nodes.%s.indices.search.query_current"),
+    'indices.search.query-total': Stat("counter", "nodes.%s.indices.search.query_total"),
+    'indices.search.query-time': Stat("counter", "nodes.%s.indices.search.query_time_in_millis"),
+    'indices.search.fetch-current': Stat("counter", "nodes.%s.indices.search.fetch_current"),
+    'indices.search.fetch-total': Stat("counter", "nodes.%s.indices.search.fetch_total"),
+    'indices.search.fetch-time': Stat("counter", "nodes.%s.indices.search.fetch_time_in_millis"),
 
-## SEARCH
-STAT['indices.search.query-current'] = { "type": "gauge", "path": "nodes.%s.indices.search.query_current" }
-STAT['indices.search.query-total'] = { "type": "counter", "path": "nodes.%s.indices.search.query_total" }
-STAT['indices.search.query-time'] = { "type": "counter", "path": "nodes.%s.indices.search.query_time_in_millis" }
-STAT['indices.search.fetch-current'] = { "type": "counter", "path": "nodes.%s.indices.search.fetch_current" }
-STAT['indices.search.fetch-total'] = { "type": "counter", "path": "nodes.%s.indices.search.fetch_total" }
-STAT['indices.search.fetch-time'] = { "type": "counter", "path": "nodes.%s.indices.search.fetch_time_in_millis" }
+    ## MERGES
+    'indices.merges.current': Stat("gauge", "nodes.%s.indices.merges.current"),
+    'indices.merges.current-docs': Stat("gauge", "nodes.%s.indices.merges.current_docs"),
+    'indices.merges.current-size': Stat("bytes", "nodes.%s.indices.merges.current_size_in_bytes"),
+    'indices.merges.total': Stat("counter", "nodes.%s.indices.merges.total"),
+    'indices.merges.total-docs': Stat("gauge", "nodes.%s.indices.merges.total_docs"),
+    'indices.merges.total-size': Stat("bytes", "nodes.%s.indices.merges.total_size_in_bytes"),
+    'indices.merges.time': Stat("counter", "nodes.%s.indices.merges.total_time_in_millis"),
 
-## STORE
-STAT['indices.store.size'] = { "type": "bytes", "path": "nodes.%s.indices.store.size_in_bytes" }
+    ## REFRESH
+    'indices.refresh.total': Stat("counter", "nodes.%s.indices.refresh.total"),
+    'indices.refresh.time': Stat("counter", "nodes.%s.indices.refresh.total_time_in_millis"),
 
-# JVM METRICS #
-## MEM
-STAT['jvm.mem.heap-committed'] = { "type": "bytes", "path": "nodes.%s.jvm.mem.heap_committed_in_bytes" }
-STAT['jvm.mem.heap-used'] = { "type": "bytes", "path": "nodes.%s.jvm.mem.heap_used_in_bytes" }
-STAT['jvm.mem.non-heap-committed'] = { "type": "bytes", "path": "nodes.%s.jvm.mem.non_heap_committed_in_bytes" }
-STAT['jvm.mem.non-heap-used'] = { "type": "bytes", "path": "nodes.%s.jvm.mem.non_heap_used_in_bytes" }
+    ## CACHE
+    'indices.cache.field.eviction': Stat("counter", "nodes.%s.indices.fielddata.evictions"),
+    'indices.cache.field.size': Stat("bytes", "nodes.%s.indices.fielddata.memory_size_in_bytes"),
+    'indices.cache.filter.evictions': Stat("counter", "nodes.%s.indices.filter_cache.filter_evictions"),
+    'indices.cache.filter.size': Stat("bytes", "nodes.%s.indices.filter_cache.memory_size_in_bytes"),
 
-## THREADS
-STAT['jvm.threads.count'] = { "type": "gauge", "path": "nodes.%s.jvm.threads.count" }
-STAT['jvm.threads.peak'] = { "type": "gauge", "path": "nodes.%s.jvm.threads.peak_count" }
+    ## FLUSH
+    'indices.flush.total': Stat("counter", "nodes.%s.indices.flush.total"),
+    'indices.flush.time': Stat("counter", "nodes.%s.indices.flush.total_time_in_millis"),
 
-## GC
-STAT['jvm.gc.time'] = { "type": "counter", "path": "nodes.%s.jvm.gc.collection_time_in_millis" }
-STAT['jvm.gc.count'] = { "type": "counter", "path": "nodes.%s.jvm.gc.collection_count" }
+    # JVM METRICS #
+    ## MEM
+    'jvm.mem.heap-committed': Stat("bytes", "nodes.%s.jvm.mem.heap_committed_in_bytes"),
+    'jvm.mem.heap-used': Stat("bytes", "nodes.%s.jvm.mem.heap_used_in_bytes"),
+    'jvm.mem.non-heap-committed': Stat("bytes", "nodes.%s.jvm.mem.non_heap_committed_in_bytes"),
+    'jvm.mem.non-heap-used': Stat("bytes", "nodes.%s.jvm.mem.non_heap_used_in_bytes"),
 
-# TRANSPORT METRICS #
-STAT['transport.server_open'] = { "type": "gauge", "path": "nodes.%s.transport.server_open" }
-STAT['transport.rx.count'] = { "type": "counter", "path": "nodes.%s.transport.rx_count" }
-STAT['transport.rx.size'] = { "type": "bytes", "path": "nodes.%s.transport.rx_size_in_bytes" }
-STAT['transport.tx.count'] = { "type": "counter", "path": "nodes.%s.transport.tx_count" }
-STAT['transport.tx.size'] = { "type": "bytes", "path": "nodes.%s.transport.tx_size_in_bytes" }
+    ## THREADS
+    'jvm.threads.count': Stat("gauge", "nodes.%s.jvm.threads.count"),
+    'jvm.threads.peak': Stat("gauge", "nodes.%s.jvm.threads.peak_count"),
 
-# HTTP METRICS #
-STAT['http.current_open'] = { "type": "gauge", "path": "nodes.%s.http.current_open" }
-STAT['http.total_open'] = { "type": "gauge", "path": "nodes.%s.http.total_opened" }
+    ## GC
+    'jvm.gc.young-time': Stat("counter", "nodes.%s.jvm.gc.collectors.young.collection_time_in_millis"),
+    'jvm.gc.young-count': Stat("counter", "nodes.%s.jvm.gc.collectors.young.collection_count"),
+    'jvm.gc.old-time': Stat("counter", "nodes.%s.jvm.gc.collectors.old.collection_time_in_millis"),
+    'jvm.gc.old-count': Stat("counter", "nodes.%s.jvm.gc.collectors.old.collection_count"),
 
-# PROCESS METRICS #
-STAT['process.open_file_descriptors'] = { "type": "gauge", "path": "nodes.%s.process.open_file_descriptors" }
+    # TRANSPORT METRICS #
+    'transport.server_open': Stat("gauge", "nodes.%s.transport.server_open"),
+    'transport.rx.count': Stat("counter", "nodes.%s.transport.rx_count"),
+    'transport.rx.size': Stat("bytes", "nodes.%s.transport.rx_size_in_bytes"),
+    'transport.tx.count': Stat("counter", "nodes.%s.transport.tx_count"),
+    'transport.tx.size': Stat("bytes", "nodes.%s.transport.tx_size_in_bytes"),
+
+    # HTTP METRICS #
+    'http.current_open': Stat("gauge", "nodes.%s.http.current_open"),
+    'http.total_open': Stat("gauge", "nodes.%s.http.total_opened"),
+
+    # PROCESS METRICS #
+    'process.open_file_descriptors': Stat("gauge", "nodes.%s.process.open_file_descriptors"),
+}
+
 
 # FUNCTION: Collect stats from JSON result
 def lookup_stat(stat, json):
 
     node = json['nodes'].keys()[0]
-    val = dig_it_up(json, STAT[stat]["path"] % node )
+    val = dig_it_up(json, STATS[stat].path % node)
 
     # Check to make sure we have a valid result
     # dig_it_up returns False if no match found
@@ -125,7 +136,7 @@ def configure_callback(conf):
         else:
             collectd.warning('elasticsearch plugin: Unknown config key: %s.'
                              % node.key)
-    ES_URL = "http://" + ES_HOST + ":" + str(ES_PORT) + "/_cluster/nodes/_local/stats?http=true&process=true&jvm=true&transport=true"
+    ES_URL = "http://" + ES_HOST + ":" + str(ES_PORT) + "/_nodes/stats/transport,http,process,jvm,indices"
 
     log_verbose('Configured with host=%s, port=%s, url=%s' % (ES_HOST, ES_PORT, ES_URL))
 
@@ -144,25 +155,22 @@ def fetch_stats():
 
 def parse_stats(json):
     """Parse stats response from ElasticSearch"""
-    for name,key in STAT.iteritems():
+    for name, key in STATS.iteritems():
         result = lookup_stat(name, json)
         dispatch_stat(result, name, key)
 
 def dispatch_stat(result, name, key):
     """Read a key from info response data and dispatch a value"""
-    if not key.has_key("path"):
-        collectd.warning('elasticsearch plugin: Stat not found: %s' % key)
-        return
     if result is None:
         collectd.warning('elasticsearch plugin: Value not found for %s' % name)
         return
-    type = key["type"]
+    estype = key.type
     value = int(result)
-    log_verbose('Sending value[%s]: %s=%s' % (type, name, value))
+    log_verbose('Sending value[%s]: %s=%s' % (estype, name, value))
 
     val = collectd.Values(plugin='elasticsearch')
     val.plugin_instance = ES_CLUSTER
-    val.type = type
+    val.type = estype
     val.type_instance = name
     val.values = [value]
     val.dispatch()
