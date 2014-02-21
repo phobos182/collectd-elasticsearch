2ce824cd85385b7c38160b0e377837a9eda8a11d
doky <nokernel@gmail.com>
2014-02-21 11:00:41 +0100

    Add Elasticsearch version compatibility

--- elasticsearch.py
+++ elasticsearch.py
@@ -6,51 +6,48 @@ import urllib2
 import socket
 import collections
 
-global URL, STAT, PREFIX, ES_HOST, ES_PORT, ES_CLUSTER
-
 PREFIX = "elasticsearch"
 ES_CLUSTER = "elasticsearch"
 ES_HOST = "localhost"
 ES_PORT = 9200
+ES_VERSION = "1.0"
+ES_URL = ""
 VERBOSE_LOGGING = False
 
 Stat = collections.namedtuple('Stat', ('type', 'path'))
 
-STATS = {
+STATS_CUR = {}
 
-    ## DOCS
-    'indices.docs.count': Stat("gauge", "nodes.%s.indices.docs.count"),
-    'indices.docs.deleted': Stat("counter", "nodes.%s.indices.docs.deleted"),
-
+# DICT: ElasticSearch 1.0.0
+STATS_ES1 = {
     ## STORE
-    'indices.store.size': Stat("bytes", "nodes.%s.indices.store.size_in_bytes"),
     'indices.store.throttle-time': Stat("counter", "nodes.%s.indices.store.throttle_time_in_millis"),
 
-    ## INDEXING
-    'indices.indexing.index-total': Stat("counter", "nodes.%s.indices.indexing.index_total"),
-    'indices.indexing.index-time': Stat("counter", "nodes.%s.indices.indexing.index_time_in_millis"),
+    ##INDEXING
     'indices.indexing.index-current': Stat("counter", "nodes.%s.indices.indexing.index_current"),
-    'indices.indexing.delete-total': Stat("counter", "nodes.%s.indices.indexing.delete_total"),
-    'indices.indexing.delete-time': Stat("counter", "nodes.%s.indices.indexing.delete_time_in_millis"),
     'indices.indexing.delete-current': Stat("counter", "nodes.%s.indices.indexing.delete_current"),
 
-    ## GET
-    'indices.get.total': Stat("counter", "nodes.%s.indices.get.total"),
-    'indices.get.time': Stat("counter", "nodes.%s.indices.get.time_in_millis"),
-    'indices.get.exists-total': Stat("counter", "nodes.%s.indices.get.exists_total"),
-    'indices.get.exists-time': Stat("counter", "nodes.%s.indices.get.exists_time_in_millis"),
-    'indices.get.missing-total': Stat("counter", "nodes.%s.indices.get.missing_total"),
-    'indices.get.missing-time': Stat("counter", "nodes.%s.indices.get.missing_time_in_millis"),
+    ##GET
     'indices.get.current': Stat("counter", "nodes.%s.indices.get.current"),
 
-    ## SEARCH
+    ##SEARCH
     'indices.search.open-contexts': Stat("counter", "nodes.%s.indices.search.open_contexts"),
-    'indices.search.query-current': Stat("gauge", "nodes.%s.indices.search.query_current"),
-    'indices.search.query-total': Stat("counter", "nodes.%s.indices.search.query_total"),
-    'indices.search.query-time': Stat("counter", "nodes.%s.indices.search.query_time_in_millis"),
-    'indices.search.fetch-current': Stat("counter", "nodes.%s.indices.search.fetch_current"),
-    'indices.search.fetch-total': Stat("counter", "nodes.%s.indices.search.fetch_total"),
-    'indices.search.fetch-time': Stat("counter", "nodes.%s.indices.search.fetch_time_in_millis"),
+
+    ##CACHE
+    'indices.cache.field.eviction': Stat("counter", "nodes.%s.indices.fielddata.evictions"),
+    'indices.cache.field.size': Stat("bytes", "nodes.%s.indices.fielddata.memory_size_in_bytes"),
+    'indices.cache.filter.evictions': Stat("counter", "nodes.%s.indices.filter_cache.evictions"),
+    'indices.cache.filter.size': Stat("bytes", "nodes.%s.indices.filter_cache.memory_size_in_bytes"),
+
+    ##GC
+    'jvm.gc.time': Stat("counter", "nodes.%s.jvm.gc.collectors.young.collection_time_in_millis"),
+    'jvm.gc.count': Stat("counter", "nodes.%s.jvm.gc.collectors.young.collection_count"),
+    'jvm.gc.old-time': Stat("counter", "nodes.%s.jvm.gc.collectors.old.collection_time_in_millis"),
+    'jvm.gc.old-count': Stat("counter", "nodes.%s.jvm.gc.collectors.old.collection_count"),
+
+    ## FLUSH
+    'indices.flush.total': Stat("counter", "nodes.%s.indices.flush.total"),
+    'indices.flush.time': Stat("counter", "nodes.%s.indices.flush.total_time_in_millis"),
 
     ## MERGES
     'indices.merges.current': Stat("gauge", "nodes.%s.indices.merges.current"),
@@ -64,16 +61,72 @@ STATS = {
     ## REFRESH
     'indices.refresh.total': Stat("counter", "nodes.%s.indices.refresh.total"),
     'indices.refresh.time': Stat("counter", "nodes.%s.indices.refresh.total_time_in_millis"),
+}
 
-    ## CACHE
-    'indices.cache.field.eviction': Stat("counter", "nodes.%s.indices.fielddata.evictions"),
-    'indices.cache.field.size': Stat("bytes", "nodes.%s.indices.fielddata.memory_size_in_bytes"),
-    'indices.cache.filter.evictions': Stat("counter", "nodes.%s.indices.filter_cache.filter_evictions"),
-    'indices.cache.filter.size': Stat("bytes", "nodes.%s.indices.filter_cache.memory_size_in_bytes"),
+# DICT: ElasticSearch 0.9.x
+STATS_ES09 = {
 
-    ## FLUSH
-    'indices.flush.total': Stat("counter", "nodes.%s.indices.flush.total"),
-    'indices.flush.time': Stat("counter", "nodes.%s.indices.flush.total_time_in_millis"),
+    ##GC
+    'jvm.gc.time': Stat("counter", "nodes.%s.jvm.gc.collection_time_in_millis"),
+    'jvm.gc.count': Stat("counter", "nodes.%s.jvm.gc.collection_count"),
+
+}
+
+# DICT: Old stuff
+STATS_OLD = {
+
+    ##CACHE
+    'indices.cache.field.eviction': Stat("counter", "nodes.%s.indices.cache.field_evictions"),
+    'indices.cache.field.size': Stat("bytes", "nodes.%s.indices.cache.field_size_in_bytes"),
+    'indices.cache.filter.count': Stat("counter", "nodes.%s.indices.cache.filter_count"),
+    'indices.cache.filter.evictions': Stat("counter", "nodes.%s.indices.cache.filter_evictions"),
+    'indices.cache.filter.size': Stat("bytes", "nodes.%s.indices.cache.filter_size_in_bytes"),
+
+    ## MERGES
+    'indices.merges.current': Stat("gauge", "nodes.%s.indices.merges.current"),
+    'indices.merges.current-docs': Stat("gauge", "nodes.%s.indices.merges.current_docs"),
+    'indices.merges.current-size': Stat("bytes", "nodes.%s.indices.merges.current_size_in_bytes"),
+    'indices.merges.total': Stat("counter", "nodes.%s.indices.merges.total"),
+    'indices.merges.total-docs': Stat("gauge", "nodes.%s.indices.merges.total_docs"),
+    'indices.merges.total-size': Stat("bytes", "nodes.%s.indices.merges.total_size_in_bytes"),
+    'indices.merges.time': Stat("counter", "nodes.%s.indices.merges.total_time_in_millis"),
+
+    ## REFRESH
+    'indices.refresh.total': Stat("counter", "nodes.%s.indices.refresh.total"),
+    'indices.refresh.time': Stat("counter", "nodes.%s.indices.refresh.total_time_in_millis"),
+}
+
+# DICT: Common stuff
+STATS = {
+
+    ## DOCS
+    'indices.docs.count': Stat("gauge", "nodes.%s.indices.docs.count"),
+    'indices.docs.deleted': Stat("counter", "nodes.%s.indices.docs.deleted"),
+
+    ## STORE
+    'indices.store.size': Stat("bytes", "nodes.%s.indices.store.size_in_bytes"),
+
+    ## INDEXING
+    'indices.indexing.index-total': Stat("counter", "nodes.%s.indices.indexing.index_total"),
+    'indices.indexing.index-time': Stat("counter", "nodes.%s.indices.indexing.index_time_in_millis"),
+    'indices.indexing.delete-total': Stat("counter", "nodes.%s.indices.indexing.delete_total"),
+    'indices.indexing.delete-time': Stat("counter", "nodes.%s.indices.indexing.delete_time_in_millis"),
+
+    ## GET
+    'indices.get.total': Stat("counter", "nodes.%s.indices.get.total"),
+    'indices.get.time': Stat("counter", "nodes.%s.indices.get.time_in_millis"),
+    'indices.get.exists-total': Stat("counter", "nodes.%s.indices.get.exists_total"),
+    'indices.get.exists-time': Stat("counter", "nodes.%s.indices.get.exists_time_in_millis"),
+    'indices.get.missing-total': Stat("counter", "nodes.%s.indices.get.missing_total"),
+    'indices.get.missing-time': Stat("counter", "nodes.%s.indices.get.missing_time_in_millis"),
+
+    ## SEARCH
+    'indices.search.query-current': Stat("gauge", "nodes.%s.indices.search.query_current"),
+    'indices.search.query-total': Stat("counter", "nodes.%s.indices.search.query_total"),
+    'indices.search.query-time': Stat("counter", "nodes.%s.indices.search.query_time_in_millis"),
+    'indices.search.fetch-current': Stat("counter", "nodes.%s.indices.search.fetch_current"),
+    'indices.search.fetch-total': Stat("counter", "nodes.%s.indices.search.fetch_total"),
+    'indices.search.fetch-time': Stat("counter", "nodes.%s.indices.search.fetch_time_in_millis"),
 
     # JVM METRICS #
     ## MEM
@@ -85,12 +138,6 @@ STATS = {
     ## THREADS
     'jvm.threads.count': Stat("gauge", "nodes.%s.jvm.threads.count"),
     'jvm.threads.peak': Stat("gauge", "nodes.%s.jvm.threads.peak_count"),
-
-    ## GC
-    'jvm.gc.young-time': Stat("counter", "nodes.%s.jvm.gc.collectors.young.collection_time_in_millis"),
-    'jvm.gc.young-count': Stat("counter", "nodes.%s.jvm.gc.collectors.young.collection_count"),
-    'jvm.gc.old-time': Stat("counter", "nodes.%s.jvm.gc.collectors.old.collection_time_in_millis"),
-    'jvm.gc.old-count': Stat("counter", "nodes.%s.jvm.gc.collectors.old.collection_count"),
 
     # TRANSPORT METRICS #
     'transport.server_open': Stat("gauge", "nodes.%s.transport.server_open"),
@@ -112,7 +159,7 @@ STATS = {
 def lookup_stat(stat, json):
 
     node = json['nodes'].keys()[0]
-    val = dig_it_up(json, STATS[stat].path % node)
+    val = dig_it_up(json, STATS_CUR[stat].path % node)
 
     # Check to make sure we have a valid result
     # dig_it_up returns False if no match found
@@ -123,7 +170,7 @@ def lookup_stat(stat, json):
 
 def configure_callback(conf):
     """Received configuration information"""
-    global ES_HOST, ES_PORT, ES_URL, VERBOSE_LOGGING
+    global ES_HOST, ES_PORT, ES_URL, ES_VERSION, VERBOSE_LOGGING, STATS_CUR
     for node in conf.children:
         if node.key == 'Host':
             ES_HOST = node.values[0]
@@ -133,15 +180,22 @@ def configure_callback(conf):
             VERBOSE_LOGGING = bool(node.values[0])
         elif node.key == 'Cluster':
             ES_CLUSTER = node.values[0]
+        elif node.key == "Version":
+            ES_VERSION = node.values[0]
         else:
             collectd.warning('elasticsearch plugin: Unknown config key: %s.'
                              % node.key)
-    ES_URL = "http://" + ES_HOST + ":" + str(ES_PORT) + "/_nodes/stats/transport,http,process,jvm,indices"
+    if ES_VERSION == "1.0":
+        ES_URL = "http://" + ES_HOST + ":" + str(ES_PORT) + "/_nodes/stats/transport,http,process,jvm,indices"
+        STATS_CUR = dict(STATS.items() + STATS_ES1.items())
+    else:
+        ES_URL = "http://" + ES_HOST + ":" + str(ES_PORT) + "/_cluster/nodes/_local/stats?http=true&process=true&jvm=true&transport=true"
+        STATS_CUR = dict(STATS.items() + STATS_ES09.items())
 
-    log_verbose('Configured with host=%s, port=%s, url=%s' % (ES_HOST, ES_PORT, ES_URL))
+    log_verbose('Configured with version=%s, host=%s, port=%s, url=%s' % (ES_VERSION, ES_HOST, ES_PORT, ES_URL))
 
 def fetch_stats(): 
-    global ES_URL, ES_CLUSTER
+    global ES_CLUSTER
 
     try:
         result = json.load(urllib2.urlopen(ES_URL, timeout = 10))
@@ -155,7 +209,7 @@ def fetch_stats():
 
 def parse_stats(json):
     """Parse stats response from ElasticSearch"""
-    for name, key in STATS.iteritems():
+    for name, key in STATS_CUR.iteritems():
         result = lookup_stat(name, json)
         dispatch_stat(result, name, key)
 
